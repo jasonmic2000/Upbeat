875c517f8afd7efff01741792b28cd69ed86f33a
Jason <jason_mic2000@yahoo.com>
2020-08-20 13:05:50 +0530

    Separating functions into files

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,63 @@
+<?php
+
+function sanitizeFormPassword($inputText){
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+
+function sanitizeFormUsername($inputText){
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ","",$inputText);
+    return $inputText;
+}
+
+function sanitizeFormName($inputText){
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ","",$inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+
+
+function validateUsername($username){
+
+}
+
+function validateFirstName($firstName){
+    
+}
+
+function validateLastName($lastName){
+    
+}
+
+function validateEmails($email, $email2){
+    
+}
+
+function validatePasswords($password, $password2){
+    
+}
+
+if(isset($_POST['registerButton'])) {
+    //Register button was pressed;
+    $username = sanitizeFormUsername($_POST['username']);
+    
+    $firstName = sanitizeFormName($_POST['firstName']);
+    $lastName = sanitizeFormName($_POST['lastName']);
+    
+    $email = sanitizeFormUsername($_POST['email']);
+    $email2 = sanitizeFormUsername($_POST['email2']);
+
+    $password = sanitizeFormPassword($_POST['password']);
+    $password2 = sanitizeFormPassword($_POST['password2']);
+
+    validateUsername($username);
+    validateFirstName($firstName);
+    validateLastName($lastName);
+    validateEmails($email, $email2);
+    validatePasswords($password, $password2);
+
+}
+
+?>
--- register.php
+++ register.php
@@ -1,44 +1,8 @@
 <!DOCTYPE html>
 
 <?php
-
-function sanitizeFormPassword($inputText){
-    $inputText = strip_tags($inputText);
-    return $inputText;
-}
-
-function sanitizeFormUsername($inputText){
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ","",$inputText);
-    return $inputText;
-}
-
-function sanitizeFormName($inputText){
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ","",$inputText);
-    $inputText = ucfirst(strtolower($inputText));
-    return $inputText;
-}
-
-if(isset($_POST['loginButton'])) {
-    //Login button was pressed;
-}
-
-if(isset($_POST['registerButton'])) {
-    //Register button was pressed;
-    $username = sanitizeFormUsername($_POST['username']);
-    
-    $firstName = sanitizeFormName($_POST['firstName']);
-    $lastName = sanitizeFormName($_POST['lastName']);
-    
-    $email = sanitizeFormUsername($_POST['email']);
-    $email2 = sanitizeFormUsername($_POST['email2']);
-
-    $password = sanitizeFormPassword($_POST['password']);
-    $password = sanitizeFormPassword($_POST['password2']);
-
-}
-
+    include("includes/handlers/register-handler.php");
+    include("includes/handlers/login-handler.php");
 ?>
 
 
