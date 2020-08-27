82a40e6c8fcd989111bf8b55880952b4cd4ec59a
Jason <jason_mic2000@yahoo.com>
2020-08-27 15:29:20 +0530

    Organized index page into separate files

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<?php
+
+include("includes/config.php");
+
+//session_destroy(); //MANUAL LOGOUT
+
+if(isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+    header("Location: register.php");
+}
+
+?>
+
+<html>
+<head>
+    <title>Welcome to Upbeat!</title>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div id="maincontainer">
+
+        <div id="topContainer">
+
+            <?php include("includes/navBarContainer.php"); ?>
+
+            <div id="mainViewContainer">
+                <div id="mainContent">
