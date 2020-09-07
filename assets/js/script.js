dccc9ae424e59a966fb93a665fa1cf892222eaff
Jason <jason_mic2000@yahoo.com>
2020-09-07 19:48:03 +0530

    Added functionality to progress bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,41 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
+
+function formatTime(seconds) {
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60); //rounds down
+    var seconds = time - (minutes * 60);
+
+    var extraZero = (seconds < 10) ? "0" : "";
+
+
+    return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio) {
+    $(".progressTime.current").text(formatTime(audio.currentTime));
+    $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+    var progress = audio.currentTime / audio.duration * 100;
+    $(".playbackBar .progress").css("width", progress + "%");
+}
 
 function Audio() {
 
     this.currentlyPlaying;
     this.audio =  document.createElement('audio');
+
+    this.audio.addEventListener("canplay", function() {
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+    });
+
+    this.audio.addEventListener("timeupdate", function() {
+        if(this.duration) {
+            updateTimeProgressBar(this);
+        }
+    });
 
     this.setTrack = function(track) {
         this.currentlyPlaying = track;
@@ -17,6 +48,10 @@ function Audio() {
 
     this.pause = function () {
         this.audio.pause();
+    }
+
+    this.setTime = function(seconds) {
+        this.audio.currentTime = seconds;
     }
 
 }
