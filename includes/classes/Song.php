3962ea83a17779c2463d0fe78aa90b7246cbda1d
Jason <jason_mic2000@yahoo.com>
2020-09-12 17:38:19 +0530

    Added function to track play button

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -32,6 +32,11 @@
             return $this->title;
         }
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
         public function getArtist() {
             return new Artist($this->con, $this->artistId);
         }
