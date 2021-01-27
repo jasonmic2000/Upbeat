9d95dd4f7ab3ed7d02a16c4c61bb4d745cb0f048
Jason <jason_mic2000@yahoo.com>
2021-01-27 18:47:38 +0530

    Remove from playlist button and function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,22 @@ function openPage(url) {
 
 }
 
+function removeFromPlaylist(button, playlistId) {
+    var songId = $(button).prevAll(".songId").val();
+        
+    $.post("includes/handlers/ajax/removeFromPlaylist.php",{ playlistId: playlistId, songId: songId }).done(function(error) {
+
+        if(error != "") {
+            alert(error);
+            return;
+        }
+
+        //do something when ajax returns
+        openPage("playlist.php?id=" + playlistId);
+    })
+
+}
+
 function createPlaylist() {
     var popup = prompt("Please enter the name of your playlist");
 
